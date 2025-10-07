66d4bc4c03223b9018050524982cb683efeea619
MESBAH UDDIN BHUIYAN <23-55139-3@student.aiub.edu>
2025-10-07 12:20:16 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+﻿A,B=1,2
+c=A+B
+
+print(c)
+name=input ("Enter your name:")
+#print(name)
+
+age =int(input("Enter your age:"))
+print(name)
+print(age)
+
+
+#light=input("Enter your light:")
+#if(light=="red"):
+    #print("You are in red.")
+#elif(light=="yellow"):
+    #print("You are in yellow.")
+#else:
+   # (light=="green")
+    #print("You are in green.")
+
