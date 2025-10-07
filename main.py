151cb3ea34e733a267b6a7f0b7ca04bedbc5f131
MESBAH UDDIN BHUIYAN <23-55139-3@student.aiub.edu>
2025-10-07 12:33:50 +0600

    working

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ name=input ("Enter your name:")
 age =int(input("Enter your age:"))
 print(name)
 print(age)
-
 print(name);
 
 
-#light=input("Enter your light:")
-#if(light=="red"):
-    #print("You are in red.")
-#elif(light=="yellow"):
-    #print("You are in yellow.")
-#else:
-   # (light=="green")
-    #print("You are in green.")
+light=input("Enter your light:")
+if(light=="red"):
+    print("You are in red.")
+elif(light=="yellow"):
+    print("You are in yellow.")
+else:
+    (light=="green")
+    print("You are in green.")
 
